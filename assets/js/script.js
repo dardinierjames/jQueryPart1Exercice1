5525d0bdaea2c3e02bd8cbd725ca8b1145105a64
dardinierjames <dardinier.james@orange.fr>
2020-11-06 15:15:22 +0100

    cacherLeParagraphe

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){
+    // appel de la fonction jQuery lorsque le document est chargé et prêt
+    $('#text').hide();
+});
+
+
+// cela s'écrit aussi :
+// $(document).ready(function(){
+//     $('#text').hide();
+// });
+
+
+// ou 3ème solution écriture simplifier au max :
+// $(function(){
+    // $('#text').css('visibility','hidden'); 
+    //$('elementCiblé').css('propriétéCSS','valeur');      
+// });
